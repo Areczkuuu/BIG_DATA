083c3936fd31a49222e008e8cdbb395ccb8b9bbf
Areczkuuu <105312535+Areczkuuu@users.noreply.github.com>
2023-07-01 12:20:28 +0200

    Update procedura kategoryczne + komenatrze.sql

--- procedura kategoryczne + komenatrze.sql
+++ procedura kategoryczne + komenatrze.sql
@@ -1,17 +1,17 @@
 DROP PROCEDURE RandomCities
 
-CREATE PROCEDURE RandomCities ( @NumberOfCities INT )   -- stworzenie procedury, która zwraca losowe miasta
+CREATE PROCEDURE RandomCities ( @NumberOfCities INT )   -- stworzenie procedury, ktÃ³ra zwraca losowe miasta
 AS
 BEGIN
 	DECLARE @randomCities AS TABLE (Miasto VARCHAR(33))  -- tabela tymczasowa z wylosowanymi miastami
-    DECLARE @temp as TABLE (Miasto VARCHAR(33), sumLud INT)  -- tabela tymczasowa z miastami i sum¹ ludnoœci w tych miastach
+    DECLARE @temp as TABLE (Miasto VARCHAR(33), sumLud INT)  -- tabela tymczasowa z miastami i sumÂ¹ ludnoÅ“ci w tych miastach
     DECLARE @rand INT, @maks INT, @counter INT = 0
 
     INSERT INTO @temp
     SELECT Miasta, SUM(Ludnosc_ogolem) OVER (ORDER BY Ludnosc_ogolem DESC) AS biezac  -- tworzenie tab tymczasowej
     FROM [MiastaSlownik]
 
-    SELECT @maks = MAX(sumLud) FROM @temp    -- maksymalna wartoœæ losowania
+    SELECT @maks = MAX(sumLud) FROM @temp    -- maksymalna wartoÅ“Ã¦ losowania
     WHILE  @counter < @NumberOfCities
     BEGIN
         SET @rand = @maks * RAND()   --losowe miasta
@@ -19,7 +19,7 @@ BEGIN
         SELECT t1.Miasto
         FROM @temp AS t1 JOIN MiastaSlownik AS t2
         ON Miasto = Miasta
-        WHERE @rand BETWEEN t1.sumLud-t2.Ludnosc_ogolem AND t1.sumLud  -- wartoœæ losowa musi byæ pomiêdzy sum¹ ludnoœci poprzednich miast, a sum¹ ludnoœci bie¿¹cego miasta
+        WHERE @rand BETWEEN t1.sumLud-t2.Ludnosc_ogolem AND t1.sumLud  -- wartoÅ“Ã¦ losowa musi byÃ¦ pomiÃªdzy sumÂ¹ ludnoÅ“ci poprzednich miast, a sumÂ¹ ludnoÅ“ci bieÂ¿Â¹cego miasta
         SET @counter = @counter + 1
     END
 	SELECT * FROM @randomCities
